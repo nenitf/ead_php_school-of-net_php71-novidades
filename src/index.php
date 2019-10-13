0ee6917a08459052a6903c9db5ee0e05b8bd0123
Felipe Silva <felipe.dacs@gmail.com>
2019-10-12 22:35:13 -0300

    feat: add link para arquivos na index

--- src/index.php
+++ src/index.php
@@ -1,3 +1,10 @@
 <?php
 
+$files = scandir('.');
+foreach($files as $file) {
+    if (!is_dir($file) && $file !== "index.php"){
+        echo "<a href='/" . $file . "'>$file</a><br>";
+    }
+}
+
 phpinfo(); 
